a1409565e158eaaf6c7a4c70d4b9ed8823c3f7fa
Jerry Lin <liping.lin.us@gmail.com>
2015-05-12 10:08:45 -0400

    Push at 2015-05-12.10:08:45

--- scripts/pdl.sh
+++ scripts/pdl.sh
@@ -1,2 +1,5 @@
 rsync -avz --progress --delete /home/lplin/rdc s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
-rsync -avz --progress --delete /home/lplin/gits/lplin/tech-notes s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+rsync -avz --progress --delete --exclude '.git' /home/lplin/gits/lplin/tech-notes s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+cd $HOME/gits/lplin/tech-notes
+git commit -am "Push at "`date +%Y-%m-%d.%H:%M:%S`
+git push origin master
