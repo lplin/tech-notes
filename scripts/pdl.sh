217ca10847008b992fa882b9cf17dc1f5545689b
lplin <liping.ln@gmail.com>
2015-05-13 11:43:48 -0400

    Push at 2015-05-13.11:43:48

--- scripts/pdl.sh
+++ scripts/pdl.sh
@@ -1,5 +1,11 @@
 rsync -avz --progress --delete /home/lplin/rdc s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+echo "===> OK pushed to dlapp:rdc"
+echo
 rsync -avz --progress --delete --exclude '.git' /home/lplin/gits/lplin/tech-notes s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+echo "===> OK pushed to dlapp:tech-notes"
+echo
 cd $HOME/gits/lplin/tech-notes
 git commit -am "Push at "`date +%Y-%m-%d.%H:%M:%S`
 git push origin master
+echo "===> OK pushed to github:tech-notes"
+echo
--- scripts/sdl.sh
+++ scripts/sdl.sh
@@ -1,4 +1,14 @@
-rsync -avz --progress --delete ~/rdc ~/bak
+BAK_DIR=~/bak/rdc-`date +%a`
+mkdir -p ${BAK_DIR}
+rsync -avz --progress --delete ~/rdc ${BAK_DIR}
+echo "===> OK backed up to ${BAK_DIR}"
+echo 
 rsync -avz --progress --delete s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P/rdc /home/lplin
+echo "===> OK synced from dlapp rdc"
+echo 
 cd $HOME/gits/lplin/tech-notes
 git pull origin master
+echo "===> OK pulled from github tech-notes rdc"
+echo 
+rsync -avz --progress --delete --exclude '.git' s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P/tech-notes /home/lplin/gits/lplin
+echo "===> OK synced from dlapp tech-notes"
