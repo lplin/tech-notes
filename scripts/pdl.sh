45fdacc6f78a5f693989e83008305205b56f8425
Jerry Lin <liping.lin.us@gmail.com>
2016-02-08 17:31:00 -0500

    Add D3 In Action

--- scripts/pdl.sh
+++ scripts/pdl.sh
@@ -1,11 +1,6 @@
-rsync -avz --progress --delete /home/lplin/rdc s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+rsync -avz --progress --delete /data/rdc s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
 echo "===> OK pushed to dlapp:rdc"
 echo
-rsync -avz --progress --delete --exclude '.git' /home/lplin/gits/lplin/tech-notes s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+rsync -avz --progress --delete --exclude '.git' /data/gits/lplin/tech-notes s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
 echo "===> OK pushed to dlapp:tech-notes"
-echo
-cd $HOME/gits/lplin/tech-notes
-git commit -am "Push at "`date +%Y-%m-%d.%H:%M:%S`
-git push origin master
-echo "===> OK pushed to github:tech-notes"
 echo
--- scripts/sdl.sh
+++ scripts/sdl.sh
@@ -1,14 +1,14 @@
-BAK_DIR=~/bak/rdc-`date +%a`
+BAK_DIR=/data/bak/rdc-`date +%a`
 mkdir -p ${BAK_DIR}
-rsync -avz --progress --delete ~/rdc ${BAK_DIR}
+rsync -avz --progress --delete /data/rdc ${BAK_DIR}
 echo "===> OK backed up to ${BAK_DIR}"
 echo 
 rsync -avz --progress --delete s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P/rdc /home/lplin
 echo "===> OK synced from dlapp rdc"
 echo 
-cd $HOME/gits/lplin/tech-notes
+cd /data/gits/lplin/tech-notes
 git pull origin master
 echo "===> OK pulled from github tech-notes rdc"
 echo 
-rsync -avz --progress --delete --exclude '.git' s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P/tech-notes /home/lplin/gits/lplin
+rsync -avz --progress --delete --exclude '.git' s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P/tech-notes /data/gits/lplin
 echo "===> OK synced from dlapp tech-notes"
