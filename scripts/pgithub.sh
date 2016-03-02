aa8c8cde2c79f229ad5f4b9e59d28ea8ab92c88e
lplin <pingnet888@gmail.com>
2016-03-02 10:38:37 -0500

    Push at 2016-03-02.10:38:37

--- scripts/pgithub.sh
+++ scripts/pgithub.sh
@@ -3,6 +3,6 @@ NOTE_DIR=/data/gits/lplin/tech-notes
 ZIP_FILE=/data/bak/tech-notes-`date +%a`.7z
 cd $NOTE_DIR
 rm $ZIP_FILE
-7z a -p $ZIP_FILE *
+7z a $ZIP_FILE *
 git commit -am "Push at "`date +%Y-%m-%d.%H:%M:%S`
 git push origin master
