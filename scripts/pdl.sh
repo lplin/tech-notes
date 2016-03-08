e99f2cb8fda64ccc793a41e22b4285c78c28c453
Jerry Lin <liping.lin.us@gmail.com>
2016-03-08 16:35:29 -0500

    Push at 2016-03-08.16:35:29

--- scripts/pdl.sh
+++ scripts/pdl.sh
@@ -1,6 +1,14 @@
-rsync -avz --progress --delete /data/rdc s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+DL_DIR=s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+rsync -avz --progress --delete /data/rdc $DL_DIR
 echo "===> OK pushed to dlapp:rdc"
 echo
-rsync -avz --progress --delete --exclude '.git' /data/gits/lplin/tech-notes s1-dlapp07:/home/lplin/tests/cmf-app/RD09000P
+
+NOTE_DIR=/data/gits/lplin/tech-notes
+rsync -avz --progress --delete --exclude '.git' $NOTE_DIR $DL_DIR
 echo "===> OK pushed to dlapp:tech-notes"
 echo
+cd $NOTE_DIR
+git commit -am "Push at "`date +%Y-%m-%d.%H:%M:%S`
+git push origin master
+echo "===> OK tech-notes pushed to github.com"
+
--- scripts/pgithub.sh
+++ scripts/pgithub.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd $HOME/rdc/
-rm $HOME/gits/lplin/scm-test1/jiras.7z
-7z a -p $HOME/gits/lplin/scm-test1/jiras.7z *
-cd $HOME/gits/lplin/scm-test1
+NOTE_DIR=/data/gits/lplin/tech-notes
+ZIP_FILE=/data/bak/tech-notes-`date +%a`.7z
+cd $NOTE_DIR
+rm $ZIP_FILE
+7z a $ZIP_FILE *
 git commit -am "Push at "`date +%Y-%m-%d.%H:%M:%S`
 git push origin master
